7069dec471a0a30fe022a3925c22ed4a1c89e497
HyoukaSan <keynotyn13@mail.ru>
2026-08-19 07:58:53 +0300

    Rewrite MakeFile (in proccess)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <types.h>
+#include <GLFW/glfw3.h>
 #include <game.h>
 #include <stdio.h>
 
 int main(void)
 {
+    if(!glfwInit()) {
+        printf("Cannot be init\n");
+        return -1;
+    }
     printf("Test!\n");
     return 0;
 }
